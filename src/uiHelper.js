1fa2c295bc6bdc456830c9ad9214b9df5b089f87
sungminoh <smoh2044@gmail.com>
2025-02-12 12:34:15 -0800

    fix model select layout

--- src/uiHelper.js
+++ src/uiHelper.js
@@ -15,7 +15,7 @@ export class UIHelper {
 
   static createDragHandle() {
     return UIHelper.createSVGButton("drag-handle", `
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
+      <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#5f6368">
         <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
       </svg>`);
   }
@@ -28,7 +28,7 @@ export class UIHelper {
 
   static createDeleteButton(onClick) {
     return UIHelper.createSVGButton("delete-btn", `
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
+      <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#5f6368">
         <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
       </svg>`, (event) => {
       event.stopPropagation();
@@ -55,7 +55,7 @@ export class UIHelper {
 
   static getCopyIcon() {
     return `
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+        <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px">
           <path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/>
         </svg>
       `;
@@ -63,7 +63,7 @@ export class UIHelper {
 
   static getSuccessIcon() {
     return `
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+        <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px">
           <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
         </svg>
       `;
