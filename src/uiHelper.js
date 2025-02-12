0bc61e23e847ce9a8dd1cb3a3dbcddac8ae8c897
sungminoh <smoh2044@gmail.com>
2025-02-12 13:35:47 -0800

    copy success button

--- src/uiHelper.js
+++ src/uiHelper.js
@@ -45,7 +45,7 @@ export class UIHelper {
   }
 
   static createCopyButton(text) {
-    return UIHelper.createSVGButton(
+    const btn = UIHelper.createSVGButton(
       "copy-btn",
       UIHelper.getCopyIcon(),
       async (e) => {
@@ -53,9 +53,10 @@ export class UIHelper {
         btn.innerHTML = UIHelper.getSuccessIcon();
         setTimeout(() => {
           btn.innerHTML = UIHelper.getCopyIcon();
-        }, 2000);
+        }, 1000);
       }
     );
+    return btn;
   }
 
   static getCopyIcon() {
@@ -66,9 +67,7 @@ export class UIHelper {
 
   static getSuccessIcon() {
     return `
-        <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px">
-          <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
-        </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
       `;
   }
 }
