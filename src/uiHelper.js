e441e975403e900117fedfe3a19f933ccf15331e
sungminoh <smoh2044@gmail.com>
2025-02-12 19:56:04 -0800

    fix delete icon

--- src/uiHelper.js
+++ src/uiHelper.js
@@ -142,9 +142,7 @@ export class UIHelper {
 
   static getDeleteIcon() {
     return `
-      <svg xmlns="http://www.w3.org/2000/svg" height="18px" width="18px" viewBox="0 -960 960 960" fill="#5f6368">
-        <path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h240q33 0 56.5 23.5T680-800v480q0 33-23.5 56.5T600-240H360Zm0-80h240v-480H360v480ZM240-80q-33 0-56.5-23.5T160-160v-80h80v560h560v80H240Z"/>
-      </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
     `;
   }
   static getDeleteSweepIcon() {
